6110d3306465eef075785cd29a88cd7a97c8c98a
Rafael Ning <rafaelning@gmail.com>
2016-07-28 13:42:33 -0700

    Update Server.py

--- SmartGridSchool/Server/Server.py
+++ SmartGridSchool/Server/Server.py
@@ -9,9 +9,8 @@ from magi.util.processAgent import initializeProcessAgent
 from CommServer import ServerCommService
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG) 
+log.setLevel(logging.DEBUG)
 
-#???
 #ch = logging.StreamHandler()
 #log.addHandler(ch)
 
@@ -21,37 +20,32 @@ class Server(SharedServer):
         # Default port when port not specified in AAL 
         self.port = 55286 
     
-    # This function should start the server
+    # This function should start the server 
     def runserver(self):
         functionName = self.runserver.__name__ + "on port " + str(self.port) 
         helpers.entrylog(log, functionName, level=logging.INFO)
-        
+
         self.commServer = ServerCommService()
         self.commServer.initCommServer(self.port, self.responseHandler)
-        
+            
         helpers.exitlog(log, functionName, level=logging.INFO)
-        
+
         return True 
     
     # This function should stop the server 
     def terminateserver(self): 
         functionName = self.terminateserver.__name__
         helpers.entrylog(log, functionName, level=logging.INFO)
-        
+   
         self.commServer.stop() 
         helpers.exitlog(log, functionName, level=logging.INFO)
-        return True  
+        return True 
     
     # Dummy response handler 
     def responseHandler(self,recvdata):
-        log.info("Got info %s" %(recvdata)) 
+        log.info("Got info %s" %(rcvdata)) 
         sendstring = "Thank you" 	
-        
         return sendstring  
-    
-    
-    #def sendMSG():
-    #    self.commServer.sendOneData()
 
 def getAgent(**kwargs):
     agent = Server()
