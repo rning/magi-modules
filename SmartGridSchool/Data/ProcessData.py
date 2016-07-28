e5670bb8034a0eaa9bd7b0405768ba4817113a22
Rafael Ning <rafaelning@gmail.com>
2016-07-28 16:52:11 -0700

    Update and rename processData.py to ProcessData.py

---
+++ SmartGridSchool/Data/ProcessData.py
@@ -0,0 +1,9 @@
+
+
+class ProcessData():
+  __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+  
+  if __name__ == "__main__":
+    netElectricity = []
+    for i in range(0, 21):
+      
