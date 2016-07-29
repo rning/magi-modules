be4083a7b548e82da00f3a008f5020fe927b2365
Rafael Ning <rafaelning@gmail.com>
2016-07-28 17:21:11 -0700

    Update ProcessData.py

--- SmartGridSchool/Data/ProcessData.py
+++ SmartGridSchool/Data/ProcessData.py
@@ -1,8 +1,5 @@
-#import os
-
 class ProcessData():
   if __name__ == "__main__":
-    #__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     
     netElectricity = []
     for i in range(0, 1440):
